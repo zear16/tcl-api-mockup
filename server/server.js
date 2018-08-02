b602e2a4770674136d51edd812fb4e0490bddafa
Zear16 <zear16@gmail.com>
2018-08-02 17:34:03 +0700

    Added: GET/POST /api/login_mobile

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+// Config middleware
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.all('/api/login_mobile', (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+    res.status(200).send({});
+});
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+});
