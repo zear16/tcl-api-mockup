27a9334712e575923a94928005008e7a0adc2d87
Zear16 <zear16@gmail.com>
2018-08-03 15:19:00 +0700

    Fixed: Deploy service stop/start

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 ssh zear16@zear16.com <<EOF
 cd /home/zear16/tcl-api-mockup
-git pull
+pm2 stop tcl-api-mockup-server
+git pull origin master
 npm install
-pm2 start all
+pm2 start tcl-api-mockup-server
 exit
 EOF
