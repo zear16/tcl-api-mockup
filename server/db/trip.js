570de2ef788007a473ee631c36e6db5ac04ab0c9
Zear16 <zear16@gmail.com>
2018-08-07 10:00:07 +0700

    Added: arrvPark

--- server/db/trip.js
+++ server/db/trip.js
@@ -1,6 +1,6 @@
 const trip = {};
 
-trip.getTrip = (id, date, time, parkId, provinceId) => {
+trip.getTrip = (id, date, time, parkId, provinceId, arrvParkId, arrvProvinceId) => {
     return {
         id: id,
         date: date,
@@ -12,6 +12,16 @@ trip.getTrip = (id, date, time, parkId, provinceId) => {
             terminal: '',
             province: {
                 id: provinceId,
+                desc: ''
+            }
+        },
+        arrvPark: {
+            id: arrvParkId,
+            desc: '',
+            descEn: '',
+            terminal: '',
+            province: {
+                id: arrvProvinceId,
                 desc: ''
             }
         },
@@ -32,19 +42,19 @@ trip.getTripResult = (departureDate, returnDate) => {
         dptrTrips: {
             tripDate: departureDate,
             trips: [
-                trip.getTrip(1, departureDate, '09:00:00', 1, 24),
-                trip.getTrip(2, departureDate, '12:00:00', 1, 24),
-                trip.getTrip(3, departureDate, '15:00:00', 1, 24),
-                trip.getTrip(4, departureDate, '18:00:00', 1, 24)
+                trip.getTrip(1, departureDate, '09:00:00', 1, 24, 11, 1),
+                trip.getTrip(2, departureDate, '12:00:00', 1, 24, 11, 1),
+                trip.getTrip(3, departureDate, '15:00:00', 1, 24, 11, 1),
+                trip.getTrip(4, departureDate, '18:00:00', 1, 24, 11, 1)
             ]
         },
         rtrnTrips: {
             tripDate: returnDate,
             trips: [
-                trip.getTrip(11, returnDate, '10:30:00', 11, 1),
-                trip.getTrip(12, returnDate, '13:30:00', 11, 1),
-                trip.getTrip(13, returnDate, '16:30:00', 11, 1),
-                trip.getTrip(14, returnDate, '19:30:00', 11, 1)
+                trip.getTrip(11, returnDate, '10:30:00', 11, 1, 1, 24),
+                trip.getTrip(12, returnDate, '13:30:00', 11, 1, 1, 24),
+                trip.getTrip(13, returnDate, '16:30:00', 11, 1, 1, 24),
+                trip.getTrip(14, returnDate, '19:30:00', 11, 1, 1, 24)
             ]
         }
     };
