f8140e39c4a01c7941c14e213fb151f4431a3383
Zear16 <zear16@gmail.com>
2018-08-14 12:30:27 +0700

    Changed: Trip time format

--- server/db/trip.js
+++ server/db/trip.js
@@ -42,19 +42,19 @@ trip.getTripResult = (departureDate, returnDate) => {
         dptrTrips: {
             tripDate: departureDate,
             trips: [
-                trip.getTrip(1, departureDate, '09:00:00', 1, 24, 11, 1),
-                trip.getTrip(2, departureDate, '12:00:00', 1, 24, 11, 1),
-                trip.getTrip(3, departureDate, '15:00:00', 1, 24, 11, 1),
-                trip.getTrip(4, departureDate, '18:00:00', 1, 24, 11, 1)
+                trip.getTrip(1, departureDate, '09:00', 1, 24, 11, 1),
+                trip.getTrip(2, departureDate, '12:00', 1, 24, 11, 1),
+                trip.getTrip(3, departureDate, '15:00', 1, 24, 11, 1),
+                trip.getTrip(4, departureDate, '18:00', 1, 24, 11, 1)
             ]
         },
         rtrnTrips: {
             tripDate: returnDate,
             trips: [
-                trip.getTrip(11, returnDate, '10:30:00', 11, 1, 1, 24),
-                trip.getTrip(12, returnDate, '13:30:00', 11, 1, 1, 24),
-                trip.getTrip(13, returnDate, '16:30:00', 11, 1, 1, 24),
-                trip.getTrip(14, returnDate, '19:30:00', 11, 1, 1, 24)
+                trip.getTrip(11, returnDate, '10:30', 11, 1, 1, 24),
+                trip.getTrip(12, returnDate, '13:30', 11, 1, 1, 24),
+                trip.getTrip(13, returnDate, '16:30', 11, 1, 1, 24),
+                trip.getTrip(14, returnDate, '19:30', 11, 1, 1, 24)
             ]
         }
     };
